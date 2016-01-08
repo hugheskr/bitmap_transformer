0585893bffec92f1b22398638defd575070f71df
Kris <kristopher_hughes@yahoo.com>
2016-01-08 15:49:34 -0800

    completed project

---
+++ test/test_bit.js
@@ -0,0 +1,10 @@
+var bit = require(__dirname + '/../lib/bitmapdatareader.js');
+var expect = require('chai').expect;
+
+describe('transforming function', function() {
+  it('should loop through buffer', function() {
+    var testArr = [1, 2, 3];
+    expect(bit.transforming(0, testArr)).to.eql([254, 253, 252]);
+  });
+})
+
